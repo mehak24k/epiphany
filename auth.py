9a1d77d80bd5fbaae05e96a810de737f851708fd
Mehak Virdi <Mehak@mehaks-mbp.lan>
2020-06-01 17:45:44 +0800

    Added email confirmation

--- auth.py
+++ auth.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
-from . import db
-from flask_login import login_user, logout_user, login_required
+from . import db, s, mail
+from flask_login import login_user, logout_user, login_required, current_user
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+from flask_mail import Mail, Message
+from datetime import datetime
 
 auth = Blueprint('auth', __name__)
 
@@ -23,6 +26,11 @@ def login_post():
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+
+    # check if user has confirmed their email before login
+    if not user.email_confirmed:
+        flash('Please confirm your email through the confirmation link sent to your inbox before login.')
+        return redirect(url_for('auth.login'))
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
@@ -51,7 +59,36 @@ def signup_post():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('auth.login'))
+    token = s.dumps(email, salt='email-confirm')
+    msg = Message('Confirm Email', sender='epiphany.orbital@gmail.com', recipients=[email])
+    link = url_for('auth.confirm_email', token=token, _external=True)
+    msg.body = 'Your link is {}'.format(link)
+
+    mail.send(msg)
+
+    new_user.email_confirmation_sent_on = datetime.now()
+    db.session.commit()
+    return redirect(url_for('auth.email_sent'))
+
+@auth.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=3600)
+    except SignatureExpired:
+        return '<h1>The token is expired!</h1>'
+    return redirect(url_for('auth.email_confirmed', email=email))
+
+@auth.route('/email_sent')
+def email_sent():
+    return render_template('email_sent.html')
+
+@auth.route('/email_confirmed')
+def email_confirmed():
+    user = db.session.query(User).filter_by(email=request.args.get('email')).first()
+    user.email_confirmed_on = datetime.now()
+    user.email_confirmed = True
+    db.session.commit()
+    return render_template('email_confirmed.html', email=request.args.get('email'))
 
 @auth.route('/logout')
 @login_required
