0ca9cde92d8378ca6bf124e5871acfc09d75421d
Yuxi <liuyuxi.yx@gmail.com>
2020-06-04 21:02:57 +0800

    Edited Post class

--- models.py
+++ models.py
@@ -15,6 +15,6 @@ class Post(db.Model):
     __tablename__="posts" 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users2.id'), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.now())
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
