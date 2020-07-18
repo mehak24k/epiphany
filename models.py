cacc7df05ee720237701e22453c06399e9fb610d
Yuxi <liuyuxi.yx@gmail.com>
2020-07-18 12:20:30 +0800

    Missed out a bracket

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class Post(db.Model):
     __tablename__="posts"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users2.id'), nullable=False)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.now
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now())
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
     category_id = db.Column(db.Integer(), db.ForeignKey('categories.id'))
