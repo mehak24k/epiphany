07189c2698965fba4f010ce7e03e635c33912956
Yuxi <liuyuxi.yx@gmail.com>
2020-07-23 20:38:09 +0800

    Finished reply functionality

--- blog.py
+++ blog.py
@@ -64,7 +64,7 @@ def indiv_post(post_id):
         tags.append({'name': tag.name})
 
     for comment in comments:
-        comm.append({'text': comment.text, 'commentor': comment.user.name, 'comment_id': comment.id, 'comment_level': comment.level()})
+        comm.append({'text': comment.text, 'commentor': comment.user.name, 'comment_id': comment.id, 'comment_level': comment.level(), 'time': comment.timestamp})
 
     json_post = {'id': post[0].id, 'user_email': post[0].user.email, 'title': post[0].title, 'body': post[0].body, 'tags': tags, 'comments': comm}
 
@@ -119,12 +119,34 @@ def comment_options():
     return jsonify({'response': response}), 205
 
 @bp.route('/posts/<int:post_id>/comment', methods=['POST'])
+@cross_origin()
 def comment(post_id):
     comment = request.get_json(force=True)
     text = comment['text']
     user_id = User.query.filter_by(email=comment['user_email']).first().id
     post_id = comment['post_id']
     new_comment = Comment(text=text, user_id=user_id, post_id=post_id)
+    new_comment.save()
+
+    response = []
+    return jsonify({'response': response}), 204
+
+@bp.route('/posts/<int:post_id>/<int:comment_id>/reply', methods=['OPTIONS'])
+@cross_origin()
+def reply_options(): 
+    response = {'hello'}
+    return jsonify({'response': response}), 205
+
+@bp.route('/posts/<int:post_id>/<int:comment_id>/reply', methods=['POST'])
+@cross_origin()
+def reply(post_id, comment_id): 
+    reply = request.get_json(force=True)
+    text=reply['text']
+    user_id = User.query.filter_by(email=reply['user_email']).first().id
+    post_id = reply['post_id']
+    parent_id = reply['parent_id']
+    parent = Comment.query.get(parent_id)
+    new_comment = Comment(text=text, user_id=user_id, post_id=post_id, parent=parent)
     new_comment.save()
 
     response = []
--- frontend/src/Post/Post.js
+++ frontend/src/Post/Post.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+  
+import React, {Component, useState} from 'react';
 import { Redirect } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
@@ -9,6 +10,7 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Media from 'react-bootstrap/Media'
 import Form from 'react-bootstrap/Form'
+import Collapse from 'react-bootstrap/Collapse'
 
 class Post extends Component {
   constructor(props) {
@@ -17,13 +19,20 @@ class Post extends Component {
       post: null,
       text:'',
       deleted: false,
+      reply:'',
     };
     this.submit = this.submit.bind(this);
     this.updateComment = this.updateComment.bind(this);
     this.deletePost = this.deletePost.bind(this);
+    this.replyTo = this.replyTo.bind(this);
+    this.Reply = this.Reply.bind(this);
   }
 
   async componentDidMount() {
+    this.refreshPost();
+  }
+
+  async refreshPost() {
     const { match: { params } } = this.props;
     const post = (await axios.get(`http://localhost:5000/posts/${params.postId}`)).data;
     this.setState({
@@ -35,6 +44,7 @@ class Post extends Component {
     this.setState({
         [event.target.name]: event.target.value
       });
+    console.log(this.state.reply);
   }
 
   async submit(event) {
@@ -48,7 +58,6 @@ class Post extends Component {
       console.log('Looks like there was a problem: \n', error);
     });
     event.preventDefault();
-
   }
 
   async deletePost() {
@@ -64,6 +73,46 @@ class Post extends Component {
     }, (error) => {
       console.log('Looks like there was a problem: \n', error);
     });
+  }
+
+  async replyTo(event, id) {
+    const { match: { params } } = this.props;
+    let postData = {"text": this.state.reply, "user_email": localStorage.getItem('userEmail'), "post_id": params.postId, "parent_id": id}
+    console.log(postData);
+    axios.post(`http://localhost:5000/posts/${params.postId}/${id}/reply`, postData)
+    .then((response) => {
+      console.log(response);
+    }, (error) => {
+      console.log('Looks like there was a problem: \n', error);
+    });
+    event.preventDefault();
+  }
+
+  Reply = (props) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <>
+        <Link
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+          size="sm"
+        >
+        Reply
+        </Link>
+        <Collapse in={open}>
+          <div>
+            <Form onSubmit={(e) => this.replyTo(e, props.id)}>
+                <Form.Group controlId="text">
+                    <Form.Control as="textarea" name="reply" placeholder={props.id} reply={this.state.reply} onChange={this.updateComment} />
+                </Form.Group>
+                <Button variant="success" type="submit" size="sm">Reply</Button>
+            </Form>
+          </div>
+        </Collapse>
+      </>
+    );
   }
 
   render() {
@@ -112,7 +161,7 @@ class Post extends Component {
             <hr className="my-4" />
                 {
                   post.comments.map(comment => (
-                    comment.comment_level === 1 ? 
+                    comment.comment_level === 1 ?
                           <Media className="mt-1">
                             <Media.Body>
                               <h5>{ comment.commentor }</h5>
@@ -120,13 +169,13 @@ class Post extends Component {
                                 { comment.text.split("\n").map((i,key) => {
                                   return <div key={key}>
                                     {i}
-                                    <p><small>reply</small></p>
-                                  </div>;
+                                    <p><small> { comment.time } · <this.Reply id={ comment.comment_id }/> </small></p>
+                                    </div>;
                                 }) }
                               </p>
                             </Media.Body>
                           </Media>
-                    : comment.comment_level === 2 ? 
+                    : comment.comment_level === 2 ?
                         <Media className="ml-3">
                           <Media.Body>
                             <h5>{ comment.commentor }</h5>
@@ -134,13 +183,13 @@ class Post extends Component {
                                 { comment.text.split("\n").map((i,key) => {
                                     return <div key={key}>
                                       {i}
-                                      <p><small>reply</small></p>
+                                      <p><small>{ comment.time } · <this.Reply id={ comment.comment_id }/></small></p>
                                     </div>;
                                 }) }
                               </p>
                           </Media.Body>
                         </Media>
-                        : 
+                        :
                           <Media className="ml-5">
                             <Media.Body>
                               <h5>{ comment.commentor }</h5>
@@ -148,14 +197,14 @@ class Post extends Component {
                                   { comment.text.split("\n").map((i,key) => {
                                       return <div key={key}>
                                         {i}
-                                        <p><small>reply</small></p>
+                                        <p><small>{ comment.time } · <this.Reply id={ comment.comment_id }/></small></p>
                                       </div>;
                                   }) }
                                 </p>
                             </Media.Body>
                           </Media>
                   ))
-                } 
+                }
           </div>
         </div>
       </div>
